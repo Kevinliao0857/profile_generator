4a1415ce728749bf86def4de1b347adb0572b2f1
Kevinliao0857 <kevinliao0857@gmail.com>
2021-02-10 03:05:14 +0000

    callback hell survey

--- survey.js
+++ survey.js
@@ -13,7 +13,6 @@ rl.question('What do you think of Node.js? ', (answer) => {
           rl.question("What's your favourite thing to eat for that meal?", (favFood) => {
             rl.question("Which sport is your absolute favourite?", (sport) => {
               rl.question(" What is your superpower? In a few words, tell us what you are amazing at!", (superPower) => {
-                console.log(`Thank you for your valuable feedback: ${answer}`);
                 console.log(`${personsName} thinks Node.js is ${answer}, likes ${activity} and listens to ${music} while ${activity}. Loves ${meal} and eat's a lot of ${favFood}, and plays ${sport} afterwards. They secretly have ${superPower} abilities.`);
                 rl.close()
               });
